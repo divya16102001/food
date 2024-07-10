f2bc2ce96b5bf6bf53595397e6db8090b15e4bfe
divya16102001 <134268173+divya16102001@users.noreply.github.com>
2024-07-10 18:33:49 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/divya/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.divya
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/RecipeView.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+class RecipeView extends StatefulWidget {
+
+  String url;
+  RecipeView(this.url);
+  
+  @override
+  _RecipeViewState createState() => _RecipeViewState();
+}
+
+class _RecipeViewState extends State<RecipeView> {
+  late  String finalUrl;
+  final Completer<WebViewController> controller = Completer<WebViewController>();
+  @override
+  void initState() {
+    if(widget.url.toString().contains("http://"))
+    {
+      finalUrl = widget.url.toString().replaceAll("http://", "https://");
+    }
+    else{
+      finalUrl = widget.url;
+    }
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Food Recipe App"),
+      ),
+      body: Container(
+        child: WebView(
+          initialUrl: widget.url,
+          javascriptMode: JavaScriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController){
+            setState(() {
+              controller.complete(webViewController);
+            });
+          
+          },
+        ),
+      ),
+            );
+  }
+  
+  WebView({required String initialUrl, required JavaScriptMode javascriptMode, required Null Function(WebViewController webViewController) onWebViewCreated}) {}
+}
+
+
